37a04c03ec26ed7fcd12efb5528d1adac8d689cf
Allison Thackston <allison@allisonthackston.com>
2022-07-22 11:41:23 -0700

    Change tabs to spaces. (#12)

--- tests/helpers/index.d.ts
+++ tests/helpers/index.d.ts
@@ -1,17 +1,17 @@
 declare namespace Chai {
-	interface Assertion {
-		readonly haveAnEntry: Assertion;
+    interface Assertion {
+        readonly haveAnEntry: Assertion;
 
-		matchFirstRegexpOf(pattern: ProblemPatternP[]): Assertion;
-		readonly nextEntryAndPatternMatch: Assertion;
+        matchFirstRegexpOf(pattern: ProblemPatternP[]): Assertion;
+        readonly nextEntryAndPatternMatch: Assertion;
 
-		matchRegexp(pattern: ProblemPatternP | string): Assertion;
-		readonly anyNextEntry: Assertion;
+        matchRegexp(pattern: ProblemPatternP | string): Assertion;
+        readonly anyNextEntry: Assertion;
 
-		readonly patternsExhausted: Assertion;
-	}
+        readonly patternsExhausted: Assertion;
+    }
 }
 
 interface ProblemPatternP {
-	regexp: string;
+    regexp: string;
 }
