fe65cc01952caf1640defaf40158b19a38f2d8fb
Allison Thackston <allison@allisonthackston.com>
2023-11-18 12:07:29 -0800

    Simplify testing by removing fixtures (#249)

--- tests/data/gen.sh
+++ tests/data/gen.sh
@@ -1,44 +1,47 @@
 #!/bin/bash
 
+OUTPUT_DIR="output"
+DATA_DIR="$PWD/tests/data"
 DOCKER_IMAGE=${DOCKER_IMAGE:-"althack/ros2:humble-dev"}
-DOCKER_CMD="docker run --rm -v $PWD/tests/data:/workspace --user $(id -u):$(id -g) ${DOCKER_IMAGE} bash -c"
+DOCKER_CMD="docker run --rm -v $DATA_DIR:/workspace --user $(id -u):$(id -g) ${DOCKER_IMAGE} bash -c"
 
 docker pull $DOCKER_IMAGE
 
 fixture_gen () {
     local program=$1
     local file=$2
-    local output=$3
+    local output="$OUTPUT_DIR/$2.out"
     echo `$DOCKER_CMD "cd /workspace && $program $file &> $output"`
 }
 
 echo "purge old data"
-rm -v tests/data/*.txt
+rm -v -fr $DATA_DIR/$OUTPUT_DIR
+mkdir -p $DATA_DIR/$OUTPUT_DIR
 
 echo "generating cpplint"
-fixture_gen ament_cpplint cpplint_ok.cpp cpplint_ok.txt
-fixture_gen ament_cpplint cpplint_fail.cpp cpplint_fail.txt
+fixture_gen ament_cpplint cpplint_ok.cpp
+fixture_gen ament_cpplint cpplint_fail.cpp
 
 echo "generating cppcheck"
-fixture_gen ament_cppcheck cppcheck_ok.cpp cppcheck_ok.txt
-fixture_gen ament_cppcheck cppcheck_fail.cpp cppcheck_fail.txt
+fixture_gen ament_cppcheck cppcheck_ok.cpp
+fixture_gen ament_cppcheck cppcheck_fail.cpp
 
 echo "generating lint_cmake"
-fixture_gen ament_lint_cmake cmakelint_ok.cmake cmakelint_ok.txt
-fixture_gen ament_lint_cmake cmakelint_fail.cmake cmakelint_fail.txt
+fixture_gen ament_lint_cmake cmakelint_ok.cmake
+fixture_gen ament_lint_cmake cmakelint_fail.cmake
 
 echo "generating flake8"
-fixture_gen ament_flake8 flake8_ok.py flake8_ok.txt
-fixture_gen ament_flake8 flake8_fail.py flake8_fail.txt
+fixture_gen ament_flake8 flake8_ok.py
+fixture_gen ament_flake8 flake8_fail.py
 
 echo "generating mypy"
-fixture_gen ament_mypy mypy_ok.py mypy_ok.txt
-fixture_gen ament_mypy mypy_fail.py mypy_fail.txt
+fixture_gen ament_mypy mypy_ok.py
+fixture_gen ament_mypy mypy_fail.py
 
 echo "generating pep257"
-fixture_gen ament_pep257 pep257_ok.py pep257_ok.txt
-fixture_gen ament_pep257 pep257_fail.py pep257_fail.txt
+fixture_gen ament_pep257 pep257_ok.py
+fixture_gen ament_pep257 pep257_fail.py
 
 echo "generating xmllint"
-fixture_gen ament_xmllint xmllint_ok.xml xmllint_ok.txt
-fixture_gen ament_xmllint xmllint_fail.xml xmllint_fail.txt
+fixture_gen ament_xmllint xmllint_ok.xml
+fixture_gen ament_xmllint xmllint_fail.xml
--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -1,48 +1,9 @@
-import { readFileSync, promises as fsPromises } from 'fs';
+import { readFileSync } from 'fs';
 import { join } from 'path';
 
-function syncReadFile(filename: string) {
-    const result = readFileSync(join(__dirname, filename), 'utf-8');
-
-    console.log(result);
-
-    return result;
+export function getOutputLines(filename: string) {
+    const folder = 'data/output';
+    const outFilename = `${filename}.out`;
+    const result = readFileSync(join(__dirname, folder, outFilename), 'utf-8');
+    return result.trim().split('\n');
 }
-
-const cpplintOutputSuccess = syncReadFile('./data/cpplint_ok.txt');
-const cpplintOutputFail = syncReadFile('./data/cpplint_fail.txt');
-
-const cppcheckOutputSuccess = syncReadFile('./data/cppcheck_ok.txt');
-const cppcheckOutputFail = syncReadFile('./data/cppcheck_fail.txt');
-
-const cmakelintOutputSuccess = syncReadFile('./data/cmakelint_ok.txt');
-const cmakelintOutputFail = syncReadFile('./data/cmakelint_fail.txt');
-
-const flake8OutputSuccess = syncReadFile('./data/flake8_ok.txt');
-const flake8OutputFail = syncReadFile('./data/flake8_fail.txt');
-
-const mypyOutputSuccess = syncReadFile('./data/mypy_ok.txt');
-const mypyOutputFail = syncReadFile('./data/mypy_fail.txt');
-
-const pep257OutputSuccess = syncReadFile('./data/pep257_ok.txt');
-const pep257OutputFail = syncReadFile('./data/pep257_fail.txt');
-
-const xmllintOutputSuccess = syncReadFile('./data/xmllint_ok.txt');
-const xmllintOutputFail = syncReadFile('./data/xmllint_fail.txt');
-
-export const fixtures = {
-    cpplintOutputSuccess,
-    cpplintOutputFail,
-    cppcheckOutputSuccess,
-    cppcheckOutputFail,
-    cmakelintOutputSuccess,
-    cmakelintOutputFail,
-    flake8OutputSuccess,
-    flake8OutputFail,
-    mypyOutputSuccess,
-    mypyOutputFail,
-    pep257OutputSuccess,
-    pep257OutputFail,
-    xmllintOutputSuccess,
-    xmllintOutputFail,
-};
